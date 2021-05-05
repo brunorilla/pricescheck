ab0c9486ec73f64d49f8fb1dc334e4716d7da891
Bruno Rilla <brilla@Lenovo.com>
2021-05-05 00:38:40 -0300

    db driver update

--- src/dbdriver.js
+++ src/dbdriver.js
@@ -1,9 +1,13 @@
-import { default as mongodb } from 'mongodb';
+import {default as mongodb} from 'mongodb';
+
 let MongoClient = mongodb.MongoClient;
 const uri = "mongodb+srv://admin:admin@branalanacluster.rjrre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
     const collection = client.db("universidad").collection("alumnos");
-    console.log(collection);
-    client.close();
+    collection.find({}).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        client.close().then(()=> console.log("Conexión cerrada"));
+    });
 });
